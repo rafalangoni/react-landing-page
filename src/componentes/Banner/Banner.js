38e77bd2d05d11a336c3a739c41306fc26f4cab4
Rafael Langoni <langoninaves@gmail.com>
2022-07-19 18:06:41 -0300

    arquivos iniciais

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+function Banner(){
+    return(
+        <header>
+            <img src="/imagens/banner2.png" alt="logo do site com chaves e chapolin"  className="banner__imagem"></img>
+        </header>
+    )
+}
+
+export default Banner
