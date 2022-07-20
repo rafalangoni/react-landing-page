b6f500fbad5c52d3bbf45aa76595bc99ee996529
Rafael Langoni <langoninaves@gmail.com>
2022-07-20 13:25:51 -0300

    componente botao do formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,19 +1,28 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () => {
     
-    const desenho = ['Chaves', 'Chapolin'];
+    const desenho = ['Chaves', 'Chapolin']
+
+    const aoSalvar =  (evento) =>{
+        evento.preventDefault();
+        console.log("form submetido")
+    }
 
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do personagem</h2>
-                <CampoTexto label="Desenho" placeholder="(Chaves ou Chapolin)." />
-                <CampoTexto label="Personagem" placeholder="Informe o personagem." />
+                <CampoTexto obrigatorio={true} label="Desenho" placeholder="(Chaves ou Chapolin)." />
+                <CampoTexto obrigatorio={true} label="Personagem" placeholder="Informe o personagem." />
                 <CampoTexto label="Imagem" placeholder="Selecione a imagem do personagem." />
-                <ListaSuspensa label="Desenho" itens={desenho}/>
+                <ListaSuspensa obrigatorio={true} label="Desenho" itens={desenho}/>
+                <Botao>
+                    Criar card
+                </Botao>
             </form>
         </section>
     );
